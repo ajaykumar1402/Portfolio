b532abae4a3194a50081fe8761957baddfbe310b
ajaykumar1402 <69111276+ajaykumar1402@users.noreply.github.com>
2024-06-19 16:15:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const typed = new Typed(".text", {
+    strings: ["Frontend Developer","Backend Developer","FullStack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+  });
+  
